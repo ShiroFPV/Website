4974ad7cee8b37adea6fd50d25a63d1ca444cdab
ShiroFPV <161849319+ShiroFPV@users.noreply.github.com>
2026-03-24 00:15:44 +0100

    test

--- betaflight-support-explorer-but-shirofied/svelte.config.js
+++ betaflight-support-explorer-but-shirofied/svelte.config.js
@@ -3,15 +3,20 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter(),
-		// This section tells Vite what your shortcuts mean
-		alias: {
-			$components: 'src/components'
-		}
-	}
+    kit: {
+        // We add an empty object or specific config here to override defaults
+        adapter: adapter({
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
+        }),
+        alias: {
+            $components: 'src/components'
+        }
+    }
 };
 
 export default config;
