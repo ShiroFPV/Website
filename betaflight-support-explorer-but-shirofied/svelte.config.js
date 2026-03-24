ab2202673c26f42e64475ae73495a3b37175427e
Shiro <manuel@chuniriders.ch>
2026-03-24 08:32:49 +0100

    Update svelte.config.js

--- betaflight-support-explorer-but-shirofied/svelte.config.js
+++ betaflight-support-explorer-but-shirofied/svelte.config.js
@@ -1,11 +1,16 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from 'path'; // Add this
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter() // Ensure this is the cloudflare adapter!
+		adapter: adapter(),
+		alias: {
+			// This tells Vite that $components points to your folder
+			$components: 'src/components', 
+		}
 	}
 };
 
