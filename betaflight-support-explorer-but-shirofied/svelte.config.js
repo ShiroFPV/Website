6fb84bcb0b589ddfff27ab491c56ba35358b16e7
Shiro <manuel@chuniriders.ch>
2026-03-25 08:35:21 +0100

    fahhh

--- betaflight-support-explorer-but-shirofied/svelte.config.js
+++ betaflight-support-explorer-but-shirofied/svelte.config.js
@@ -1,21 +1,17 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
-    kit: {
-        adapter: adapter(),
-        alias: {
-            $components: path.resolve(__dirname, './src/components')
-        }
-    }
+	kit: {
+		// adapter-cloudflare is the correct choice for Cloudflare Pages
+		adapter: adapter(),
+		alias: {
+			$components: 'src/components'
+		}
+	}
 };
 
 export default config;
