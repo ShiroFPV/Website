e1f740cc0c50456a1af3a8fd7f68a143d98dcc1d
Shiro <manuel@chuniriders.ch>
2026-03-25 08:22:07 +0100

    get it working on cloudflare

--- betaflight-support-explorer-but-shirofied/svelte.config.js
+++ betaflight-support-explorer-but-shirofied/svelte.config.js
@@ -1,13 +1,21 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter(),
+        alias: {
+            $components: path.resolve(__dirname, './src/components')
+        }
+    }
 };
 
 export default config;
